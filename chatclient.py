247f5330b406b54503c9e9141f6ef55dc72532ab
Edgar Fong <edgarfong25@hotmail.com>
2023-04-16 19:42:51 -0700

    Q will no longer exit app, will enter menu again.

--- chatclient.py
+++ chatclient.py
@@ -9,69 +9,69 @@ from datetime import datetime
 host_name = "127.0.1.1"
 port = 18000
 
-#prompts menu with 3 options
-print("Please select one of the following options:")
-print("1. Get a report of the chatroom from the server.")
-print("2. Request to join the chatroom.")
-print("3. Quit the program.")
-choice = input()
-print("your choice:", choice)
-
-
-# Prompts the client for a username
-if choice == "2":
-
-    # Creates the TCP socket
-    new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Connecting to", host_name, port, "...")
-    new_socket.connect((host_name, port))
-    print("Connected.")
-
-    print("Type lowercase 'q' at anytime to quit!")
-    name = input("Enter your a username: ")
-elif choice == 1:
-    #do option 1, adding exit in the meantime
-    exit()
-else:
-    exit()
-
-
-
-# Thread to listen for messages from the server
 def listen_for_messages():
     while True:
-        message = new_socket.recv(1024).decode()
+        try:
+            message = new_socket.recv(1024).decode()
+        except ConnectionAbortedError:
+            break
         print("\n" + message)
 
-
-t = Thread(target=listen_for_messages)
-
-t.daemon = True
-
-t.start()
-
-# if user is an admin send the admin name before appending time and username
-if name == "admin":
-    print("Welcome Administrator")
-    new_socket.send(name.encode())
-
 while True:
-    # Recieves input from the user for a message
-    to_send = input()
+    #prompts menu with 3 options
+    print("Please select one of the following options:")
+    print("1. Get a report of the chatroom from the server.")
+    print("2. Request to join the chatroom.")
+    print("3. Quit the program.")
+    choice = input()
+    print("your choice:", choice)
 
-    # Allows the user to exit the chat room
-    if to_send.lower() == "q":
-        new_socket.send(to_send.encode())
+    # Prompts the client for a username
+    if choice == "2":
+        # Creates the TCP socket
+        new_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Connecting to", host_name, port, "...")
+        new_socket.connect((host_name, port))
+        print("Connected.")
+
+        print("Type lowercase 'q' at anytime to quit!")
+        name = input("Enter your a username: ")
+
+        # Thread to listen for messages from the server
+        t = Thread(target=listen_for_messages)
+        t.daemon = True
+        t.start()
+
+        # if user is an admin send the admin name before appending time and username
+        if name == "admin":
+            print("Welcome Administrator")
+            new_socket.send(name.encode())
+
+        while True:
+            # Recieves input from the user for a message
+            to_send = input()
+
+            # Allows the user to exit the chat room
+            if to_send.lower() == "q":
+                new_socket.send(to_send.encode())
+                break
+            # Appends the username and time to the clients message
+            to_send = name + ": " + to_send
+            date_now = datetime.now().strftime("[%H:%M] ")
+            to_send = date_now + to_send
+
+            # Sends the message to the server
+            new_socket.send(to_send.encode())
+
+        # close the socket
+        new_socket.close()
+
+    elif choice == "1":
+        #do option 1, adding exit in the meantime
         exit()
-    # Appends the username and time to the clients message
-    to_send = name + ": " + to_send
-    date_now = datetime.now().strftime("[%H:%M] ")
-    to_send = date_now + to_send
+        menu() #prints the menu again
+    else:
+        exit()
 
-    # Sends the message to the server
-    new_socket.send(to_send.encode())
-
-# close the socket
-new_socket.close()
 
 #AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
